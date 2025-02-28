9d0b8fa3b68322aeeb16f0928b20b691c29381e3
墨染_nlx <78503566+morannlx@users.noreply.github.com>
2025-03-01 00:45:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,4 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
-git clone https://github.com/Zxilly/UA2F package/UA2F
-git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/huhu415/uaProxy.git package/uaProxy
+git clone https://github.com/morannlx/curl-openwrt.git package/curl
